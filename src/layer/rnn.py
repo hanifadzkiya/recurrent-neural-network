27b6bda00e884492a483bfaca061f9a525115a3d
faizmuh <13517093@std.stei.itb.ac.id>
2020-11-06 13:43:46 +0700

    hidden layer

--- src/layer/rnn.py
+++ src/layer/rnn.py
@@ -1,22 +1,35 @@
+import numpy as np
+
 def softmax(x):
     """ applies softmax to an input x"""
     e_x = np.exp(x)
     return e_x / e_x.sum()
 
 class RNN():
-    def __init__(self):
+    def __init__(self, inputs, hidden_state):
         self.output = []
-        self.b_hy = []
-        self.v = []
+        self.hidden = []
+        self.u = np.random.uniform(-1.0/np.sqrt(len(inputs)), 1.0/np.sqrt(len(inputs)), (hidden_state, len(inputs)))
+        self.w = np.random.uniform(-1.0/np.sqrt(hidden_state), 1.0/np.sqrt(hidden_state), (hidden_state, hidden_state))
+        self.v = np.random.uniform(-1.0/np.sqrt(hidden_state), 1.0/np.sqrt(hidden_state), (len(inputs), hidden_state))
+        self.b_xh = np.zeros(hidden_state, 1)
+        self.b_hy = np.zeros(len(inputs), 1)
 
     def forward(self, inputs):
         last_hidden = np.zero() ## inisialisasi hidden t0 zero
         for input in inputs:
-            last_hidden = hidden(input)
+            last_hidden = self.hidden(input, last_hidden)
             output(last_hidden)
 
-    def hidden(self, input):
-        # return hidden result
+    def hidden(self, inputs, last_hidden):
+        uxt = np.dot(self.u, inputs)
+        net = uxt + (np.dot(self.w, last_hidden) + self.b_xh)
+        hidden = np.tanh(net)
+    
+        # Disimpan untuk kebutuhan backprop (jika ada)
+        self.hidden.append(hidden)
+        
+        return hidden
 
     def output(self, hidden):
         output = softmax(self.v * hidden + self.b_hy)
